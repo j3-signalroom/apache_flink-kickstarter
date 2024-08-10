c4883cc7cc9aaac1738ec9125e7c95d657693bd1
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-10 18:01:45 +0200

    See 8.  Refined script.

--- scripts/run-flink-locally.sh
+++ scripts/run-flink-locally.sh
@@ -11,43 +11,57 @@
 if [ ! -n "$1" ]
 then
     echo
-    echo "Usage:  Require one argument ---> `basename $0` --profile=<PROFILE_NAME>"
+    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<CHIP_NAME>"
     echo
     exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
 fi
 
 # Get the arguments passed
+aws_sso=false
+flink_docker_image=''
 arg_count=0
 for arg in "$@" # $@ sees arguments as separate words
 do
     case $arg in
         *"--profile="*)
-            arg_length=10
-            environment_name=${arg:$arg_length:$(expr ${#arg} - $arg_length)}
             AWS_PROFILE=$arg;;
+        *"--chip=amd64"*)
+            flink_docker_image='j3signalroom/linux_flink-with_hadoop_iceberg:latest';;
+        *"--chip=arm64"*)
+            flink_docker_image='j3signalroom/mac_flink-with_hadoop_iceberg:latest';;
+        *"--aws-sso"*)
+            aws_sso=true;;
     esac
     let "arg_count+=1"
 done
 
-if [ "$arg_count" -lt "1" ]
+if [ "$arg_count" -lt "2" ]
 then
     echo
-    echo "Usage:  Require the ---> `basename $0` --profile=<PROFILE_NAME>"
+    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<CHIP_NAME>"
     echo
     exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
 fi
 
-# Get the SSO AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, AWS_SESSION_TOKEN, and AWS_REGION, and
-# set them as an environmental variables
-aws sso login $AWS_PROFILE
-eval $(aws2-wrap $AWS_PROFILE --export)
-export AWS_REGION=$(aws configure get sso_region $AWS_PROFILE)
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+# Check if the --aws-sso flag was supplied
+if [ "$aws_sso" = true ]
+then
+    # Get the SSO AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, AWS_SESSION_TOKEN, and AWS_REGION, and
+    # set them as an environmental variables
+    aws sso login $AWS_PROFILE
+    eval $(aws2-wrap $AWS_PROFILE --export)
+    export AWS_REGION=$(aws configure get sso_region $AWS_PROFILE)
+    export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
-# Create and then pass the AWS environment variables to docker-compose
-printf "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}\
-\nAWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}\
-\nAWS_REGION=${AWS_REGION}\
-\nAWS_DEFAULT_REGION=${AWS_REGION}\
-\nFLINK_DOCKER_IMAGE='j3signalroom/mac_flink-with_hadoop_iceberg:latest'" > .env
+    # Create and then pass the AWS environment variables to docker-compose
+    printf "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}\
+    \nAWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}\
+    \nAWS_REGION=${AWS_REGION}\
+    \nAWS_DEFAULT_REGION=${AWS_REGION}\
+    \nFLINK_DOCKER_IMAGE=${flink_docker_image} > .env
+else
+    # Create and then pass the AWS environment variables to docker-compose
+    printf "FLINK_DOCKER_IMAGE=${flink_docker_image} > .env
+fi
+
 docker-compose up 
