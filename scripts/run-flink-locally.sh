127521261cd494ffc02dfbdfb97ffc98ec942fdb
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-10 18:36:04 +0200

    See 8.  Refined script.

--- scripts/run-flink-locally.sh
+++ scripts/run-flink-locally.sh
@@ -2,16 +2,18 @@
 
 #
 # *** Script Syntax ***
-# scripts/run-flink-locally.sh --profile=<PROFILE_NAME>
+# scripts/run-flink-locally.sh --profile=<PROFILE_NAME> --chip=<amd64 | arm64> [--aws-sso]
 #
 # *** Example Call ***
-# scripts/run-flink-locally.sh --profile=dev
+# scripts/run-flink-locally.sh --profile=AdministratorAccess-0123456789 --chip=arm64 --aws-sso
 #
 # Check if arguments were supplied; otherwise exit script
 if [ ! -n "$1" ]
 then
     echo
-    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<CHIP_NAME>"
+    echo "(Error Message 001)  You did not include any arguments in the call."
+    echo
+    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<amd64 | arm64> [--aws-sso]"
     echo
     exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
 fi
@@ -38,12 +40,25 @@ done
 if [ "$arg_count" -lt "2" ]
 then
     echo
-    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<CHIP_NAME>"
+    echo "(Error Message 002)  You did not include all the required arguments in the call."
+    echo
+    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<amd64 | arm64> [--aws-sso]"
     echo
     exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
 fi
 
-# Check if the --aws-sso flag was supplied
+# Check if the required --chip argument was supplied
+if [ -z $flink_docker_image ]
+then
+    echo
+    echo "(Error Message 003)  You did not include the property use of the --chip=<amd64 | arm64> argument in the call."
+    echo
+    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<PROFILE_NAME> --chip=<amd64 | arm64> [--aws-sso]"
+    echo
+    exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
+fi
+
+# Check if the [--aws-sso] flag was supplied
 if [ "$aws_sso" = true ]
 then
     # Get the SSO AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, AWS_SESSION_TOKEN, and AWS_REGION, and
@@ -58,10 +73,11 @@ then
     \nAWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}\
     \nAWS_REGION=${AWS_REGION}\
     \nAWS_DEFAULT_REGION=${AWS_REGION}\
-    \nFLINK_DOCKER_IMAGE=${flink_docker_image} > .env
+    \nFLINK_DOCKER_IMAGE=${flink_docker_image}" > .env
 else
     # Create and then pass the AWS environment variables to docker-compose
-    printf "FLINK_DOCKER_IMAGE=${flink_docker_image} > .env
+    printf "FLINK_DOCKER_IMAGE=${flink_docker_image}" > .env
 fi
 
-docker-compose up 
+# Run the Apache Flink cluster containers in the background
+docker-compose up -d
