3ad6e9a45c6aced18cba0915449702490549402a
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-11 03:55:51 +0200

    Resolved #26.

--- scripts/force-aws-secrets-destroy.sh
+++ scripts/force-aws-secrets-destroy.sh
@@ -17,3 +17,35 @@ then
     echo
     exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
 fi
+
+# Get the arguments passed
+arg_count=0
+for arg in "$@" # $@ sees arguments as separate words
+do
+    case $arg in
+        *"--profile="*)
+            AWS_PROFILE=$arg;;
+    esac
+    let "arg_count+=1"
+done
+
+# Check required --profile argument was supplied
+if [ -z $AWS_PROFILE ]
+then
+    echo
+    echo "(Error Message 002)  You did not include the proper use of the --profile=<AWS_SSO_PROFILE_NAME> argument in the call."
+    echo
+    echo "Usage:  Require at least two arguments ---> `basename $0` --profile=<AWS_SSO_PROFILE_NAME>"
+    echo
+    exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
+fi
+
+# Get the SSO AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, AWS_SESSION_TOKEN, and AWS_REGION, and
+# set them as an environmental variables
+aws sso login $AWS_PROFILE
+eval $(aws2-wrap $AWS_PROFILE --export)
+export AWS_REGION=$(aws configure get sso_region $AWS_PROFILE)
+
+# Force the destroy of all the AWS Secrets created by the Terraform configuration
+aws secretsmanager delete-secret --secret-id /confluent_cloud_resource/schema_registry_cluster/java_client --force-delete-without-recovery
+aws secretsmanager delete-secret --secret-id /confluent_cloud_resource/kafka_cluster/java_client --force-delete-without-recovery
--- scripts/run-flink-locally.sh
+++ scripts/run-flink-locally.sh
@@ -72,7 +72,6 @@ fi
 aws sso login $AWS_PROFILE
 eval $(aws2-wrap $AWS_PROFILE --export)
 export AWS_REGION=$(aws configure get sso_region $AWS_PROFILE)
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 
 # Create and then pass the AWS environment variables to docker-compose
 printf "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}\
