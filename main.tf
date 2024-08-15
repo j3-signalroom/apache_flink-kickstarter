6068a548395b337cc36537f0c31095e19a9df63f
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-16 00:52:40 +0200

    See #51.  Refactored.

--- main.tf
+++ main.tf
@@ -26,9 +26,6 @@ provider "confluent" {
 
 provider "aws" {
     region     = var.aws_region
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
-    token      = var.aws_session_token
 }
 
 locals {
