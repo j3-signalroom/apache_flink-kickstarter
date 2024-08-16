da95b8551a00d7286ccb8cc7f324a8966c0611c1
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-16 10:44:12 +0200

    Resolved #60.

--- aws-resources.tf
+++ aws-resources.tf
@@ -124,5 +124,5 @@ resource "aws_ssm_parameter" "producer_kafka_client_acks" {
 }
 
 resource "aws_s3_bucket" "iceberg_bucket" {
-  bucket = "iceberg-bucket"
+  bucket = "apache-flink-kickstarter-iceberg-bucket"
 }
--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "confluent" {
 
 provider "aws" {
     region     = var.aws_region
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
-    token      = var.aws_session_token
+    #access_key = var.aws_access_key_id
+    #secret_key = var.aws_secret_access_key
+    #token      = var.aws_session_token
 }
