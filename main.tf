177978d06fbda6e63a6078ed2d28195fbf0c0516
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-16 08:37:54 +0200

    Conforming to Terraform best practices for structuring a Terraform project.  The provider blocks have been moved to the provider.tf file.

--- main.tf
+++ main.tf
@@ -19,15 +19,6 @@ terraform {
     }
 }
 
-provider "confluent" {
-  cloud_api_key    = var.confluent_cloud_api_key
-  cloud_api_secret = var.confluent_cloud_api_secret
-}
-
-provider "aws" {
-    region     = var.aws_region
-}
-
 locals {
   cloud          = "AWS"
   secrets_prefix = "/confluent_cloud_resource"
