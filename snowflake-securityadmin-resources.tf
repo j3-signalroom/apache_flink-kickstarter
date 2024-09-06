46e72ccce7c7b01840bf957e4edd221510916c0c
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-09-05 23:12:32 -0400

    See #61.  Initial commit.

---
+++ snowflake-securityadmin-resources.tf
@@ -0,0 +1,77 @@
+provider "snowflake" {
+  alias         = "security_admin"
+  role          = "SECURITYADMIN"
+  account       = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["account"]
+  user          = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["admin_user"]
+  authenticator = "JWT"
+  private_key   = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["active_rsa_public_key_number"] == 1 ? data.aws_secretsmanager_secret_version.admin_private_key_1.secret_string : data.aws_secretsmanager_secret_version.admin_private_key_2.secret_string
+}
+
+resource "snowflake_role" "role" {
+  provider = snowflake.security_admin
+  name     = "example_role"
+}
+
+resource "snowflake_grant_privileges_to_account_role" "database_grant" {
+  provider          = snowflake.security_admin
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_role.role.name
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = snowflake_database.example.name
+  }
+}
+
+resource "snowflake_schema" "schema" {
+  database   = snowflake_database.example.name
+  name       = "example_data_lakehouse"
+}
+
+resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
+  provider          = snowflake.security_admin
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_role.role.name
+  on_schema {
+    schema_name = "\"${snowflake_database.example.name}\".\"${snowflake_schema.schema.name}\""
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
+  provider          = snowflake.security_admin
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_role.role.name
+  on_account_object {
+    object_type = "WAREHOUSE"
+    object_name = snowflake_warehouse.example.name
+  }
+}
+
+resource "snowflake_user" "user" {
+  provider          = snowflake.security_admin
+  name              = var.service_account_user
+  default_warehouse = snowflake_warehouse.example.name
+  default_role      = snowflake_role.role.name
+  default_namespace = "${snowflake_database.example.name}.${snowflake_schema.schema.name}"
+
+  # Setting the attributes to `null`, effectively unsets the attribute
+  # Refer to this link `https://docs.snowflake.com/en/user-guide/key-pair-auth#configuring-key-pair-rotation`
+  # for more information
+  rsa_public_key    = module.snowflake_user_rsa_key_pairs_rotation.active_rsa_public_key_number == 1 ? jsondecode(data.aws_secretsmanager_secret_version.svc_public_keys.secret_string)["rsa_public_key_1"] : null
+  rsa_public_key_2  = module.snowflake_user_rsa_key_pairs_rotation.active_rsa_public_key_number == 2 ? jsondecode(data.aws_secretsmanager_secret_version.svc_public_keys.secret_string)["rsa_public_key_2"] : null
+}
+
+resource "snowflake_grant_privileges_to_account_role" "user_grant" {
+  provider          = snowflake.security_admin
+  privileges        = ["MONITOR"]
+  account_role_name = snowflake_role.role.name  
+  on_account_object {
+    object_type = "USER"
+    object_name = snowflake_user.user.name
+  }
+}
+
+resource "snowflake_grant_account_role" "grants" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_role.role.name
+  user_name = snowflake_user.user.name
+}
---
+++ snowflake-sysadmin-resources.tf
@@ -0,0 +1,17 @@
+provider "snowflake" {
+  role          = "SYSADMIN"
+  account       = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["account"]
+  user          = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["admin_user"]
+  authenticator = "JWT"
+  private_key   = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["active_rsa_public_key_number"] == 1 ? data.aws_secretsmanager_secret_version.admin_private_key_1.secret_string : data.aws_secretsmanager_secret_version.admin_private_key_2.secret_string
+}
+
+resource "snowflake_database" "example" {
+  name = "example"
+}
+
+resource "snowflake_warehouse" "example" {
+  name           = "example"
+  warehouse_size = "xsmall"
+  auto_suspend   = 60
+}
