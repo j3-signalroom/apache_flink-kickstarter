da492b515c9a15ef43bf0d7c9c491c6f7ea82d88
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-09-22 13:51:17 -0400

    See #206.  Completed the 3 initial tasks.

--- python/kickstarter/helper/kafka_properties.py
+++ python/kickstarter/helper/kafka_properties.py
@@ -1,12 +1,9 @@
-from typing import Iterator
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.table import StreamTableEnvironment
-from pyflink.table.expressions import call, col
-from pyflink.common import Types, Row
-from pyflink.table import DataTypes, TableEnvironment, EnvironmentSettings, Schema, TableDescriptor
-from pyflink.table.udf import udtf, udf
 import json
-from re import sub
+from typing import Iterator
+from pyflink.table.expressions import col
+from pyflink.common import Row
+from pyflink.table import DataTypes
+from pyflink.table.udf import udtf
 
 from aws_services import AwsServices
 
@@ -18,7 +15,11 @@ __email__      = "j3@thej3.com"
 __status__     = "dev"
 
 
-def get_kafka_properties(cluster_secrets_path: str, client_parameters_path: str) -> tuple[str, str | None]:
+_gbl_for_consumer = True
+_gbl_service_account_user = "tf_snowflake_user"
+
+
+def get_kafka_properties_from_aws(cluster_secrets_path: str, client_parameters_path: str) -> tuple[str, str | None]:
     """This method returns the Kafka Cluster properties from the AWS Secrets Manager and Parameter Store.
 
     Args:
@@ -55,24 +56,15 @@ def get_kafka_properties(cluster_secrets_path: str, client_parameters_path: str)
             return None
     else:
         return None
-    
-
-
-# Set up the environment
-env = StreamExecutionEnvironment.get_execution_environment()
-tbl_env = StreamTableEnvironment.create(env)
-
-# Adjust resource configuration
-env.set_parallelism(1)  # Set parallelism to 1 for simplicity
 
 @udtf(result_types=DataTypes.ROW([DataTypes.FIELD('property_key',DataTypes.STRING()),
-                                 DataTypes.FIELD('property_value', DataTypes.STRING())]))
+                                    DataTypes.FIELD('property_value', DataTypes.STRING())]))
 def kafka_properties_udtf(kakfa_properties: Row) -> Iterator[Row]:
     # Get the Kafka Client properties from AWS Secrets Manager and AWS Systems Manager Parameter Store.
-    for_consumer = True
-    service_account_user = "tf_snowflake_user"
+    for_consumer = _gbl_for_consumer
+    service_account_user = _gbl_service_account_user
     secret_path_prefix = f"/confluent_cloud_resource/{service_account_user}"
-    properties = get_kafka_properties(
+    properties = get_kafka_properties_from_aws(
         f"{secret_path_prefix}/kafka_cluster/java_client",
         f"{secret_path_prefix}/consumer_kafka_client" if for_consumer else f"{secret_path_prefix}/producer_kafka_client"
     )
@@ -82,43 +74,44 @@ def kafka_properties_udtf(kakfa_properties: Row) -> Iterator[Row]:
         for property_key, property_value in properties.items():
             yield Row(str(property_key), str(property_value))
 
-# Seed table with empty data
-example_data = [('','')]
+def get_kafka_properties(tbl_env, for_consumer: bool, service_account_user: str) -> dict[str, str]:
+    global _gbl_for_consumer, _gbl_service_account_user
+    _gbl_for_consumer = for_consumer
+    _gbl_service_account_user = service_account_user
 
-# Define the schema for the table
-schema = DataTypes.ROW([
-    DataTypes.FIELD('property_key', DataTypes.STRING()),
-    DataTypes.FIELD('property_value', DataTypes.STRING())
-])
+    # Define the schema for the table
+    schema = DataTypes.ROW([
+        DataTypes.FIELD('property_key', DataTypes.STRING()),
+        DataTypes.FIELD('property_value', DataTypes.STRING())
+    ])
 
-# Create a table from the empty and the provide schema
-kafka_property_table = tbl_env.from_elements(example_data, schema)
+    # Create an empty table
+    kafka_property_table = tbl_env.from_elements([('','')], schema)
 
-# Register the table as a temporary view
-tbl_env.create_temporary_view('kafka_property_table', kafka_property_table)
+    # Register the table as a temporary view
+    tbl_env.create_temporary_view('kafka_property_table', kafka_property_table)
 
-kafka_property_table = tbl_env.from_path('kafka_property_table')
-print('\n Kafka Property Table Schema:--->')
-kafka_property_table.print_schema()
+    kafka_property_table = tbl_env.from_path('kafka_property_table')
 
-print('\n Kafka Property Table Data:--->')
-device_stats = kafka_property_table.join_lateral(kafka_properties_udtf.alias("key", "value")).select(col("key"), col("value"))
-device_stats.execute().print()
+    # print('\n Kafka Property Table Schema:--->')
+    # kafka_property_table.print_schema()
 
-# Convert the result into a Python dictionary
-# First, collect the results using to_data_stream or execute.collect()
-result = device_stats.execute().collect()
+    func_results = kafka_property_table.join_lateral(kafka_properties_udtf.alias("key", "value")).select(col("key"), col("value"))
+    # print('\n Kafka Property Table Data:--->')
+    # func_results.execute().print()
 
-# Process the results into a Python dictionary
-result_dict = {}
-for row in result:
-    if ".ms" in row[0]:
-        """Convert the string representation of milliseconds into its float-point value"""
-        result_dict[row[0]] = int(row[1])
-    else:
-        result_dict[row[0]] = row[1]
+    # Convert the result into a Python dictionary
+    result = func_results.execute().collect()
+    result_dict = {}
+    for row in result:
+        if ".ms" in row[0]:
+            # Convert the string representation of milliseconds into its float-point value
+            result_dict[row[0]] = int(row[1])
+        else:
+            result_dict[row[0]] = row[1]
 
-result.close()
+    result.close()
 
-# Now you have the table result in a dictionary
-print(result_dict)
+    # Return the table results into a dictionary
+    # print(result_dict)
+    return result_dict
