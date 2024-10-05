0ad1b1de7bb0b7ee815bf3e246647605535761da
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-10-05 05:08:56 -0400

    See #259.  Updated the S3 bucket IAM policy.

--- aws-resources.tf
+++ aws-resources.tf
@@ -137,6 +137,40 @@ resource "aws_s3_bucket_public_access_block" "iceberg_bucket_public_access_block
   restrict_public_buckets = true
 }
 
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "iceberg_bucket-s3-policy"
+  description = "Policy to allow Flink access to S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}"
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
+  # The role name is extracted from the ARN of the current caller
+  role       = "aws_iam_role.${local.sso_role_name}.name"
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
 data "aws_secretsmanager_secret" "admin_public_keys" {
   name = "/snowflake_admin_credentials"
 }
