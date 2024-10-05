8a31a32d587a0be94f251ad4238f80f6ac63686a
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-10-05 10:02:18 -0400

    See #259.  The error message indicates that the role AWSReservedSSO_AdministratorAccess_3c09ce30b5d743b7 is a protected role managed by AWS SSO and cannot be modified directly. To attach an IAM policy to a role, you need to create a new IAM role or use an existing IAM role that is not managed by AWS SSO.

--- aws-resources.tf
+++ aws-resources.tf
@@ -137,39 +137,49 @@ resource "aws_s3_bucket_public_access_block" "iceberg_bucket_public_access_block
   restrict_public_buckets = true
 }
 
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "iceberg_bucket-s3-policy"
-  description = "Policy to allow Flink access to S3 bucket"
+resource "aws_iam_role" "iceberg_bucket_role" {
+  name = "iceberg_bucket_role"
 
-  policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-          "s3:ListBucket",
-          "s3:GetBucketLocation"
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}"
-      },
-      {
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}/*"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-data "aws_caller_identity" "current" {}
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "iceberg_bucket-s3-policy"
+  description = "Policy for accessing the S3 bucket"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = "s3:ListBucket"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}"
+      }
+    ]
+  })
+}
 
 resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
-  # The role name is extracted from the ARN of the current caller
-  role       = regex("role/([^/]*)/", data.aws_caller_identity.current.arn)[0]
+  role       = aws_iam_role.iceberg_bucket.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
