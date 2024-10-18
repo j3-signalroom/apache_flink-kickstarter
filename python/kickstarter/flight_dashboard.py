680520289b56d4d93db55ce1fc83a3e44a16d052
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-10-18 15:30:22 -0400

    See #31.  Initial code complete.

--- python/kickstarter/flight_dashboard.py
+++ python/kickstarter/flight_dashboard.py
@@ -1,17 +1,11 @@
 import streamlit as st
-from pyflink.common import WatermarkStrategy
-from pyflink.datastream import StreamExecutionEnvironment, DataStream
-from pyflink.datastream.connectors.kafka import KafkaSource, KafkaSink, KafkaRecordSerializationSchema, KafkaOffsetsInitializer, DeliveryGuarantee
-from pyflink.datastream.formats.json import JsonRowDeserializationSchema, JsonRowSerializationSchema
+from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import TableEnvironment, EnvironmentSettings, StreamTableEnvironment
-from pyflink.table.table_schema import TableSchema
 from pyflink.table.catalog import ObjectPath
 from pyflink.table.expressions import *
-from datetime import datetime, timezone
 import argparse
 import pandas as pd
 
-from model.flight_data import FlightData
 from helper.utilities import catalog_exist
 
 __copyright__  = "Copyright (c) 2024 Jeffrey Jonathan Jennings"
@@ -28,18 +22,28 @@ def main(args):
     Args:
         args (str): is the arguments passed to the script.
     """
+    # --- Create a blank Flink execution environment
     env = StreamExecutionEnvironment.get_execution_environment()
+
+    # --- Enable checkpointing every 5000 milliseconds (5 seconds)
     env.enable_checkpointing(5000)
+
+    #
+    # Set timeout to 60 seconds
+    # The maximum amount of time a checkpoint attempt can take before being discarded.
+    #
     env.get_checkpoint_config().set_checkpoint_timeout(60000)
+
+    #
+    # Set the maximum number of concurrent checkpoints to 1 (i.e., only one checkpoint
+    # is created at a time)
+    #
     env.get_checkpoint_config().set_max_concurrent_checkpoints(1)
 
-    tbl_env = StreamTableEnvironment.create(env, environment_settings=EnvironmentSettings.new_instance().in_streaming_mode().build())
+    # Create a Table Environment
+    tbl_env = StreamTableEnvironment.create(stream_execution_environment=env, environment_settings=EnvironmentSettings.new_instance().in_streaming_mode().build())
 
-
-    # Define the CREATE CATALOG Flink SQL statement to register the Iceberg catalog
-    # using the HadoopCatalog to store metadata in AWS S3 (i.e., s3a://), a Hadoop- 
-    # compatible filesystem.  Then execute the Flink SQL statement to register the
-    # Iceberg catalog
+    # Create the Apache Iceberg catalog with integration with AWS Glue back by AWS S3
     catalog_name = "apache_kickstarter"
     bucket_name = args.s3_bucket_name.replace("_", "-") # To follow S3 bucket naming convention, replace underscores with hyphens if exist
     try:
@@ -47,8 +51,11 @@ def main(args):
             tbl_env.execute_sql(f"""
                 CREATE CATALOG {catalog_name} WITH (
                     'type' = 'iceberg',
-                    'catalog-type' = 'in-memory',            
-                    'warehouse' = 's3a://{bucket_name}/warehouse'
+                    'warehouse' = 's3://{bucket_name}/warehouse',
+                    'catalog-impl' = 'org.apache.iceberg.aws.glue.GlueCatalog',
+                    'io-impl' = 'org.apache.iceberg.aws.s3.S3FileIO',
+                    'glue.skip-archive' = 'True',
+                    'glue.region' = '{args.aws_region}'
                     );
             """)
         else:
@@ -60,30 +67,39 @@ def main(args):
     # Use the Iceberg catalog
     tbl_env.use_catalog(catalog_name)
 
-    # Access the Iceberg catalog to create the airlines database and the Iceberg tables
+    # Access the Iceberg catalog to query the airlines database
     catalog = tbl_env.get_catalog(catalog_name)
 
     # Print the current catalog name
     print(f"Current catalog: {tbl_env.get_current_catalog()}")
-    print(tbl_env.list_catalogs())
 
     # Check if the database exists.  If not, create it
     database_name = "airlines"
-    tbl_env.use_database(database_name)
-    
+    try:
+        if not catalog.database_exists(database_name):
+            tbl_env.execute_sql(f"CREATE DATABASE IF NOT EXISTS {database_name};")
+        else:
+            print(f"The {database_name} database already exists.")
+        tbl_env.use_database(database_name)
+    except Exception as e:
+        print(f"A critical error occurred to during the processing of the database because {e}")
+        exit(1)
 
     # Print the current database name
     print(f"Current database: {tbl_env.get_current_database()}")
-    print(tbl_env.list_databases())
 
-    flight_table_path = ObjectPath(database_name, "skyone_airline")
+    table_name = "flight"
+    flight_table_path = ObjectPath(database_name, table_name)
 
-    print(f"Flight table path: {flight_table_path}")
-    print(tbl_env.list_tables())
+    # Read data from the Iceberg table
+    result_table = tbl_env.sql_query(f"""
+        SELECT * FROM {database_name}.{table_name}
+    """)
 
-    print(catalog.get_table(flight_table_path).get_detailed_description())
-    print(catalog.get_table_statistics(flight_table_path).get_row_count())
-    print(catalog.get_table_statistics(flight_table_path).get_field_count())
+    df = result_table.to_pandas()
+    
+    st.dataframe(df)
+
 
 
 
@@ -93,5 +109,9 @@ if __name__ == "__main__":
                         dest='s3_bucket_name',
                         required=True,
                         help='The AWS S3 bucket name.')
+    parser.add_argument('--aws-region',
+                        dest='aws_region',
+                        required=True,
+                        help='The AWS Rgion name.')
     known_args, _ = parser.parse_known_args()
     main(known_args)
