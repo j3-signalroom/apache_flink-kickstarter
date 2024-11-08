b776cbe9c21602d509e90a4cb8cf9082dc515b02
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-11-08 09:15:12 -0500

    Resolved #441..

---
+++ python/tests/kickstarter/test_flight_importer_app.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import datetime
+import pytest
+
+# --- Add the source directory containing the 'model' package to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/kickstarter')))
+
+from test_helpers import AirlineFlightDataBuilder
+
+
+def test_define_workflow_should_convert_data_from_two_streams():
+    sky_one_flight = AirlineFlightDataBuilder().build()
+    sunset_flight = AirlineFlightDataBuilder().build()
+
+    # Mocking the workflow: combining two streams into one list (as we don't have Flink environment in pytest)
+    combined_stream = [sky_one_flight, sunset_flight]
+
+    expected_data = [
+        sky_one_flight,
+        sunset_flight
+    ]
+
+    assert len(combined_stream) == len(expected_data)
+    assert all(item in combined_stream for item in expected_data)
+
+
+def test_define_workflow_should_filter_out_flights_in_the_past():
+    add_minute_to_now = (datetime.datetime.now() + datetime.timedelta(minutes=1)).strftime("%Y-%m-%d %H:%M:%S")
+    subtract_second_to_now = (datetime.datetime.now() - datetime.timedelta(seconds=1)).strftime("%Y-%m-%d %H:%M:%S")
+
+    new_sky_one_flight = AirlineFlightDataBuilder().set_arrival_time(add_minute_to_now).build()
+    old_sky_one_flight = AirlineFlightDataBuilder().set_arrival_time(subtract_second_to_now).build()
+    new_sunset_flight = AirlineFlightDataBuilder().set_arrival_time(add_minute_to_now).build()
+    old_sunset_flight = AirlineFlightDataBuilder().set_arrival_time(subtract_second_to_now).build()
+
+    # Mocking the workflow: filtering out flights in the past
+    combined_stream = [new_sky_one_flight, old_sky_one_flight, new_sunset_flight, old_sunset_flight]
+    filtered_stream = [flight for flight in combined_stream if datetime.datetime.strptime(flight.arrival_time, "%Y-%m-%d %H:%M:%S") > datetime.datetime.now()]
+
+    expected_data = [
+        new_sky_one_flight,
+        new_sunset_flight
+    ]
+
+    assert len(filtered_stream) == len(expected_data)
+    assert all(item in filtered_stream for item in expected_data)
