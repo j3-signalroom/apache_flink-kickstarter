72c6e1b96e50bb65a1fc0390634f8ea5ac16bfe5
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-11-10 08:21:53 -0500

    See #444.  Renamed module.

--- python/src/kickstarter/helper/kafka_schema_registry.py
+++ python/src/kickstarter/helper/kafka_schema_registry.py
@@ -12,7 +12,7 @@ __email__      = "j3@thej3.com"
 __status__     = "dev"
 
 
-class SchemaRegistry:
+class KafkaSchemaRegistry:
     """Kafka Schema Registry class."""
     
     def __init__(self, service_account_user: str):
@@ -26,20 +26,19 @@ class SchemaRegistry:
             the prefix to the path of the Schema Registry Cluster secrets in the AWS Secrets Manager.
         """
         
+        # --- Set the service account user
         self._service_account_user = service_account_user
         
-        #
-        self.aws_service = AwsConfluentProperties()
-
-        #
+        # --- Get the Schema Registry properties
         schema_registry_path = f"/confluent_cloud_resource/{self._service_account_user}/schema_registry_cluster/python_client"
-        self._schema_registry_properties = self.get_schema_registry_properties(schema_registry_path)
+        confluent_properties = AwsConfluentProperties()
+        self._schema_registry_properties = confluent_properties.get_schema_registry_properties(schema_registry_path)
         if self._schema_registry_properties is None:
             raise RuntimeError(f"Failed to retrieve the Schema Registry properties from '{schema_registry_path}' secrets because {self._schema_registry_properties.get_error_message_code()}:{self._schema_registry_properties.get_error_message()}")
     
     def create_delete_if_exist_schema(self, subject_name: str, schema_type: str, schema_string: str) -> (any):
         """
-        This method deletes schema (and all its versions), if exist.  Then Takes the `schema_string`, 
+        This method deletes schema (and all its versions), if exist.  Then takes the `schema_string`, 
         saves it as the `schema_type`, and puts it in in the Schema Registry as a new entry.
         
         Arg(s):
@@ -53,17 +52,17 @@ class SchemaRegistry:
         """
         
         # Connect to the Schema Registry
-        self.client = SchemaRegistryClient(self._schema_registry_properties)
+        self._client = SchemaRegistryClient(self._schema_registry_properties)
         
         # Delete schema and all versions of it
         with suppress(Exception):
-            self.client.delete_subject(subject_name, True)
+            self._client.delete_subject(subject_name, True)
             
         # Converts schema into a byte array for transport over the wire
         schema = Schema(schema_string, schema_type)
         
         # Registers the schema with the Schema Registry
-        schema_id = self.client.register_schema(subject_name, schema)
+        schema_id = self._client.register_schema(subject_name, schema)
         logging.info('%s Avro Schema ID: %d', subject_name, schema_id)
         
         return schema_id
@@ -71,5 +70,5 @@ class SchemaRegistry:
     def get_schema_registry_client(self):
         """Get the Schema Registry Client."""
         
-        return self.client
+        return self._client
     
