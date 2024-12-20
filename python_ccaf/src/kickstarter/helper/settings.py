d9b23df43e81d87f56c19393755a772265b5b0cf
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-12-20 09:39:48 -0500

    Resolved #584.

--- python_ccaf/src/kickstarter/helper/settings.py
+++ python_ccaf/src/kickstarter/helper/settings.py
@@ -1,5 +1,9 @@
 import os
-
+import boto3
+from botocore.exceptions import ClientError
+import json
+import logging
+from typing import Dict
 
 __copyright__  = "Copyright (c) 2024 Jeffrey Jonathan Jennings"
 __credits__    = ["Jeffrey Jonathan Jennings"]
@@ -10,3 +14,21 @@ __status__     = "dev"
 
 
 CC_PROPERTIES_PATHNAME = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../../config/cc.properties")
+
+def get_secrets(self, aws_region_name: str, secrets_name: str) -> Dict[str, str]:
+    """This method retrieve secrets from the AWS Secrets Manager.
+    
+    Arg(s):
+        aws_region_name (str): Pass the AWS region name.
+        secrets_name (str): Pass the name of the secrets you want the secrets for.
+        
+    Return(s):
+        If successful, the secrets in a dict.  Otherwise, returns an empty dict.
+    """
+    client = self._session.client(service_name='secretsmanager', region_name=aws_region_name)        
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secrets_name)
+        return json.loads(get_secret_value_response['SecretString'])
+    except ClientError as e:
+        logging.error("Failed to get secrets (%s) from the AWS Secrets Manager because of %s.", secrets_name, e)
+        return {}
