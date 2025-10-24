d4b797461e244325ab8f929f73898afe5a103d4d
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-10-24 10:03:58 -0400

    Resolved #722.

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
         }
         aws = {
             source  = "hashicorp/aws"
-            version = "6.17.0"
+            version = "6.18.0"
         }
         snowflake = {
             source = "snowflakedb/snowflake"
