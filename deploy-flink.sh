901906624d38b45657b7a735fa8864d6d30b777d
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-11-02 11:06:52 -0500

    See #798.  Refactored.

--- deploy-flink.sh
+++ deploy-flink.sh
@@ -3,7 +3,7 @@
 #
 # *** Script Syntax ***
 # ./deploy-flink.sh <on | off> --profile=<AWS_SSO_PROFILE_NAME>
-#                              --chip=<amd64 | arm64>
+#                              --chip=<amd64 | amd64-extend | arm64 | arm64-extend>
 #                              --flink-language=<python | java>
 #
 #
@@ -18,16 +18,17 @@ case $1 in
     echo
     echo "(Error Message 001)  You did not specify one of the commands: <on | off>."
     echo
-    echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | arm64> --flink-language=<python | java>"
+    echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | amd64-extend | arm64 | arm64-extend> --flink-language=<python | java>"
     echo
     exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
     ;;
 esac
 
 # Get the arguments passed
-AWS_S3_BUCKET=""
 use_non_mac=true
 chip_arg_provider=false
+extend_resources=false
+
 # Get the arguments passed by shift to remove the first word
 # then iterate over the rest of the arguments
 shift
@@ -36,15 +37,22 @@ do
     case $arg in
         *"--profile="*)
             AWS_PROFILE=$arg;;
-        *"--aws-s3-bucket="*)
-            arg_length=16
-            aws_s3_bucket=${arg:$arg_length:$(expr ${#arg} - $arg_length)};;
         --chip=amd64)
             chip_arg_provider=true
-            use_non_mac=true;;
+            use_non_mac=true
+            extend_resources=false;;
+        --chip=amd64-extend)
+            chip_arg_provider=true
+            use_non_mac=true
+            extend_resources=true;;
         --chip=arm64)
             chip_arg_provider=true
-            use_non_mac=false;;
+            use_non_mac=false
+            extend_resources=false;;
+        --chip=arm64-extend)
+            chip_arg_provider=true
+            use_non_mac=false
+            extend_resources=true;;
         --flink-language=python)
             language_arg_provider=true
             FLINK_LANGUAGE="python";;
@@ -62,7 +70,7 @@ then
         echo
         echo "(Error Message 002)  You did not include the proper use of the --profile=<AWS_SSO_PROFILE_NAME> argument in the call."
         echo
-        echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | arm64> --flink-language=<python | java>"
+        echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | amd64-extend | arm64 | arm64-extend> --flink-language=<python | java>"
         echo
         exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
     fi
@@ -71,9 +79,9 @@ then
     if [ $chip_arg_provider = false ]
     then
         echo
-        echo "(Error Message 003)  You did not include the proper use of the --chip=<amd64 | arm64> argument in the call."
+        echo "(Error Message 003)  You did not include the proper use of the --chip=<amd64 | amd64-extend | arm64 | arm64-extend> argument in the call."
         echo
-        echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | arm64> --flink-language=<python | java>"
+        echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | amd64-extend | arm64 | arm64-extend> --flink-language=<python | java>"
         echo
         exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
     fi
@@ -84,7 +92,7 @@ then
         echo
         echo "(Error Message 004)  You did not include the proper use of the --flink-language=<python | java> argument in the call."
         echo
-        echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | arm64> --flink-language=<python | java>"
+        echo "Usage:  Require ---> `basename $0` <on | off> --profile=<AWS_SSO_PROFILE_NAME> --chip=<amd64 | amd64-extend | arm64 | arm64-extend> --flink-language=<python | java>"
         echo
         exit 85 # Common GNU/Linux Exit Code for 'Interrupted system call should be restarted'
     fi
@@ -95,14 +103,14 @@ then
     eval $(aws2-wrap $AWS_PROFILE --export)
     export AWS_REGION=$(aws configure get sso_region $AWS_PROFILE)
 
-    # Gets s3_bucket_warehouse_name of the AWS S3 Bucket created during the apply run
-    s3_bucket_warehouse_name=$(terraform output -raw s3_bucket_warehouse_name)
+    # Gets aws_s3_bucket of the AWS S3 Bucket created during the apply run
+    aws_s3_bucket=$(terraform output -raw s3_bucket_warehouse_name)
 
     # Check if the s3_bucket_warehouse_name contains the word "warning", because the output
     # variable may not exist
-    if echo "$s3_bucket_warehouse_name" | grep -iq "warning"
+    if echo "$aws_s3_bucket" | grep -iq "warning"
     then
-       s3_bucket_warehouse_name="" 
+       aws_s3_bucket="" 
     fi
 
     # Gets service_account_user of the Service Account User created during the apply run
@@ -124,20 +132,40 @@ then
     \nAWS_DEFAULT_REGION=${AWS_REGION}\
     \nAWS_PROFILE=${AWS_PROFILE}\
     \nSERVICE_ACCOUNT_USER=${service_account_user}\
-    \nAWS_S3_BUCKET=${s3_bucket_warehouse_name}" > .env
+    \nAWS_S3_BUCKET=${aws_s3_bucket}" > .env
 
     # Deploy the Docker containers via docker-compose based on the platform
     if [ $use_non_mac = true ]
     then
-        docker-compose -f linux-docker-compose.yml up -d 
+        if [ $extend_resources = true ]
+        then
+            docker-compose -f linux-docker-compose-extend.yml up -d 
+        else
+            docker-compose -f linux-docker-compose.yml up -d 
+        fi
     else
-        docker-compose -f mac-docker-compose.yml up -d
+        if [ $extend_resources = true ]
+        then
+            docker-compose -f mac-docker-compose-extend.yml up -d 
+        else
+            docker-compose -f mac-docker-compose.yml up -d
+        fi
     fi
 else
     if [ $use_non_mac = true ]
     then
-        docker-compose -f linux-docker-compose.yml down
+        if [ $extend_resources = true ]
+        then
+            docker-compose -f linux-docker-compose-extend.yml down
+        else
+            docker-compose -f linux-docker-compose.yml down
+        fi
     else
-        docker-compose -f mac-docker-compose.yml down
+        if [ $extend_resources = true ]
+        then
+            docker-compose -f mac-docker-compose-extend.yml down
+        else
+            docker-compose -f mac-docker-compose.yml down
+        fi
     fi
 fi
